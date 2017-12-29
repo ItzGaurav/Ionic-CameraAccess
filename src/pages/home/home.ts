a648552ceb7a13c96e01a94bad96e3dd9cebf53c
itzGaurav <gaurav20100@gmail.com>
2017-12-30 00:11:05 +0530

    Camera access in Ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,64 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public photos:any;
+  public base64Img:string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera,public alertCtrl: AlertController) {
 
   }
 
+ngOnInit(){
+  this.photos=[];
+
+}
+
+  takePhoto(){
+   
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.photos.push(base64Image);
+     this.photos.reverse();
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+  deletePhoto(index){
+    let confirm = this.alertCtrl.create({
+      title: 'Sure you want to delete this picture?',
+      message: '',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('No clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.photos.splice(index,1);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 }
